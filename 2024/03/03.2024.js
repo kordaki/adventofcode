bb96502397dc5f117149dbd249f1643b9f94537c
Pouriya Kordaki <pouriya.kordaki@justeattakeaway.com>
2024-12-03 23:44:43 +0100

    03.2024 two

--- 2024/03/03.2024.js
+++ 2024/03/03.2024.js
@@ -15,13 +15,41 @@ async function readFile() {
 const first = async () => {
   const input = await readFile();
   const regexPattern = /mul\((\d+),(\d+)\)/g;
-  let matches = 0;
+  let sum = 0;
   let match;
   while ((match = regexPattern.exec(input)) !== null) {
-    matches += (+match[1]) * (+match[2]);
+    sum += (+match[1]) * (+match[2]);
   }
 
-  console.log(matches);
+  console.log(sum);
 };
 
-first();
+////////////////////////////
+
+const isMull = (text) => text.startsWith('mul')
+const isDo = (text) => text === "do()"
+const isDont = (text) => text === "don't()"
+
+const regexMul = /mul\((\d+),(\d+)\)/;
+
+const second = async () => {
+  const input = await readFile();
+  const regexSanitizing = /mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/g;
+  const matches = input.match(regexSanitizing);
+
+  let enabled = true
+  let result = 0;
+  matches.forEach(item => {
+    if(isDont(item)) {
+      enabled = false
+    } else if(isDo(item)){
+      enabled = true
+    } else if(enabled && isMull(item)){
+      const match = item.match(regexMul);
+      result += (+match[1]) * (+match[2]);
+    }
+  })
+  console.log(result)
+};
+
+second()
