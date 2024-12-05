3231b22e644302e612c3b39b5b73b38b15abde9e
Pouriya Kordaki <pouriya.kordaki@justeattakeaway.com>
2024-12-05 12:59:12 +0100

    05.2024 new version of part one

--- 2024/05/05.2024.js
+++ 2024/05/05.2024.js
@@ -2,8 +2,8 @@ const fs = require("node:fs").promises;
 
 async function readFile() {
   try {
-    const data = await fs.readFile("./05.2024.txt", "utf8");
-    // const data = await fs.readFile("./mock.txt", "utf8");
+    // const data = await fs.readFile("./05.2024.txt", "utf8");
+    const data = await fs.readFile("./mock.txt", "utf8");
     console.log("ok");
     return data;
   } catch (err) {
@@ -11,55 +11,66 @@ async function readFile() {
   }
 }
 
-const getSanitizedInput = (input) => {
-  const splitted = input.split("\n");
-  const separator = splitted.indexOf("");
-  const rules = splitted.splice(0, separator).map((order) => order.split("|"));
-  const list = splitted
-    .splice(1, splitted.length)
-    .map((items) => items.split(","));
-  return { rules, list };
-};
 const rulesDic = {};
-
-// { '75': { next: [ '29', '53', '47', '61', '13' ], before: [ '97' ] }}
-const getRulesForPage = (page, rules) => {
-  if (rulesDic[page]) return rulesDic[page];
-  const sortedRules = { next: [], before: [] };
-  rules.forEach((rule) => {
-    if (rule[0] === page) {
-      sortedRules.next.push(rule[1]);
-    } else if (rule[1] === page) {
-      sortedRules.before.push(rule[0]);
+const getSanitizedInput = (input) => {
+  const splitted = input.split("\n\n");
+  splitted[0].split("\n").forEach((items) => {
+    const [x, y] = items.split("|");
+    if (rulesDic[x]) {
+      rulesDic[x].push(y);
+    } else {
+      rulesDic[x] = [y];
     }
   });
-  rulesDic[page] = sortedRules;
-  return sortedRules;
+  const list = splitted[1].split("\n").map((items) => items.split(","));
+  return { rulesDic, list };
 };
+
+// // { '75': { next: [ '29', '53', '47', '61', '13' ], before: [ '97' ] }}
+// const getRulesForPage = (page, rules) => {
+//   if (rulesDic[page]) return rulesDic[page];
+//   const sortedRules = { next: [], before: [] };
+//   rules.forEach((rule) => {
+//     if (rule[0] === page) {
+//       sortedRules.next.push(rule[1]);
+//     } else if (rule[1] === page) {
+//       sortedRules.before.push(rule[0]);
+//     }
+//   });
+//   rulesDic[page] = sortedRules;
+//   return sortedRules;
+// };
 
 ////////////////////////////
 const first = async () => {
   const input = await readFile();
-  const { rules, list } = getSanitizedInput(input);
+  const { rulesDic, list } = getSanitizedInput(input);
   let output = 0;
 
-
   list.forEach((row) => {
+    // const row = list[3]
     let rowPass = true;
     for (let i = 0; i < row.length - 1; i++) {
-      const rulesForPage = getRulesForPage(row[i], rules);
+      const page = row[i];
+      const rulesForPage = rulesDic[page] || [];
       for (let j = i + 1; j < row.length; j++) {
-        const pagePass = rulesForPage.next.includes(row[j]) && !rulesForPage.before.includes(row[j]);
+        const pagePass = rulesForPage.includes(row[j]);
         if (!pagePass) {
+          // console.log('--- rowPass:',rowPass, row[i], row[j], rulesForPage.next, rulesForPage.before)
           rowPass = false;
+          // const temp = row[i];
+          // row[i] = row[j];
+          // row[j] = temp;
+          // i = 0;
+          // j = 1;
           break;
         }
       }
-      if(!rowPass) break;
+      // if(!rowPass) break;
     }
-    if(rowPass){
-      const mid = row[Math.floor(row.length/2)]
-      console.log('+++ rowPass:',rowPass, mid,  row)
+    if (rowPass) {
+      const mid = row[Math.floor(row.length / 2)];
+      console.log("+++ rowPass:", rowPass, mid, row);
       output += parseInt(mid);
     }
   });
@@ -69,13 +80,3 @@ const first = async () => {
 
 first();
 
-////////////////////////////
-
-const second = async () => {
-  const input = await readFile();
-  let result = 0;
-
-  console.log(result);
-};
-
-// second()
